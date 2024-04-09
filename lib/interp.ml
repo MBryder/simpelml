4986593827b0b7c553e904f9c2016480db704ed3
sigurdthecoder <101118685+sigurdthecoder@users.noreply.github.com>
2024-04-09 15:00:06 +0200

    Rework af 3 operationer, dog ikke perfekt

--- lib/interp.ml
+++ lib/interp.ml
@@ -113,26 +113,35 @@ let invert (v: value array) : value array =
   let matrix = Array.map (Array.map (function Vint x -> float_of_int x | _ -> error "Matrix elements must be integers")) matrix in
   let id = Array.init n (fun i -> Array.init n (fun j -> if i = j then 1. else 0.)) in
 
-  for i = 0 to n - 1 do
-    let pivot = matrix.(i).(i) in
-    if pivot = 0. then error "Matrix is not invertible";
-
-    (* Scale the pivot row *)
+  let rec scale_pivot_row i pivot =
     for j = 0 to n - 1 do
       matrix.(i).(j) <- matrix.(i).(j) /. pivot;
       id.(i).(j) <- id.(i).(j) /. pivot;
     done;
+  in
 
-    (* Eliminate other rows *)
-    for j = 0 to n - 1 do
-      if i <> j then
-        let factor = matrix.(j).(i) in
-        for k = 0 to n - 1 do
-          matrix.(j).(k) <- matrix.(j).(k) -. factor *. matrix.(i).(k);
-          id.(j).(k) <- id.(j).(k) -. factor *. id.(i).(k);
-        done;
-    done;
-  done;
+  let rec eliminate_other_rows i =
+    if i < n then begin
+      let pivot = matrix.(i).(i) in
+      if pivot = 0. then error "Matrix is not invertible";
+
+      scale_pivot_row i pivot;
+
+      for j = 0 to n - 1 do
+        if i <> j then begin
+          let factor = matrix.(j).(i) in
+          for k = 0 to n - 1 do
+            matrix.(j).(k) <- matrix.(j).(k) -. factor *. matrix.(i).(k);
+            id.(j).(k) <- id.(j).(k) -. factor *. id.(i).(k);
+          done;
+        end;
+      done;
+
+      eliminate_other_rows (i + 1);
+    end;
+  in
+
+  eliminate_other_rows 0;
 
   Array.map (fun row -> Vlist (Array.map (fun x -> Vint (int_of_float x)) row)) id
 
