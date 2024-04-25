baff748e15a16cde9384dc074d0929388c400573
sigurdthecoder <101118685+sigurdthecoder@users.noreply.github.com>
2024-04-25 12:43:16 +0200

    unit test updates

--- test/testModule.ml
+++ test/testModule.ml
@@ -1,22 +1,34 @@
 open Alcotest
 open Lexer
-open Parser
 
-(* Skriv om, så kan modtage en string, der indeholder flere tokens *)
-(* Lige nu kan testen kun modtage en string, der indeholder én token *)
+let rec read_all_tokens lexbuf =
+  match Lexer.next_tokens lexbuf with
+  | [IDENT _stop] -> []
+  | tokens -> List.map token_to_string tokens @ read_all_tokens lexbuf
 
-let test_lexer () =
-  let lexbuf = Lexing.from_string "+" in
-  let expected = "PLUS" in
-  let actual = 
+  let test_lexer_operator () =
+    let test_token input expected =
+          let lexbuf = Lexing.from_string input in
+          let actual = read_all_tokens lexbuf in
+          Alcotest.(check (list string)) "lexer test" expected actual
+    in
+    test_token "5 * 5 _stop" ["CST (Cint 5)"; "TIMES"; "CST (Cint 5)"];
+    test_token "M+ _stop" ["MPLUS"]
+
+let test_lexer_operator_fake () =
+  let lexbuf = Lexing.from_string "TRANS" in (* "test-filen" *) 
+    let expected = "TRANS" in (* Expected  *) 
+    let actual = 
     match Lexer.next_tokens lexbuf with
-    | [EOF] -> ""
     | tokens -> List.hd (List.map token_to_string tokens)
   in
-  check Alcotest.string "lexer test" expected actual
+  check Alcotest.string "lexer test" expected actual (* Assertion om EXPECTED er lig ACTUAL *)  
 
+    
+(* Alcotest formaliteter *)
 let suite = [
-  "test_lexer", `Quick, test_lexer;
+  "test_lexer_operator", `Quick, test_lexer_operator; (* Should pass with flying colors *)
+  "test_lexer_operator_fake", `Quick, test_lexer_operator_fake; (*Should Fail*)
 ]
 
 let () =
